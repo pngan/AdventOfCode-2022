19cc44836af098fec636c89f08a6de7fdc75cfd5
Phil Ngan <phillip.ngan@xero.com>
2026-07-11 01:12:04 +1200

    [2026 Day 08]

--- code/day08.cs
+++ code/day08.cs
@@ -1,31 +1,187 @@
-using System.Collections.Immutable;
-using System.IO;
-using System.Linq;
-using MoreLinq;
+using AdventOfCode_2022.utilities.image;
 
 namespace AdventOfCode_2022.code;
+
+
 public class Day08
 {
     public const string Day = "08";
-    public static object Input()
+    public static Im2 Input()
     {
-        var input = File.ReadLines($"input/2022_{Day}_input.txt");
-
-        return input;
+        var input = File.ReadLines($"input/2022_{Day}_input.txt").Where(s => !string.IsNullOrEmpty(s));
+        return new Im2(input);
     }
-    // 1717
+
+
     public static object Solve1()
     {
-        var input = Input();
+        int visible = 0;
+        var im = Input();
+        foreach (var p in im.Image)
+        {
 
-        return input;
+            bool n = IsVisibleN(p, im);
+            bool s = IsVisibleS(p, im);
+            bool w = IsVisibleW(p, im);
+            bool e = IsVisibleE(p, im);
+            if (n || s || w || e)
+            {
+                visible++;
+            }
+        }
+        return visible;
     }
 
-    // 321975
     public static object Solve2()
     {
-        var input = Input();
-
-        return input;
+        long result = 0;
+        var im = Input();
+        foreach (var p in im.Image)
+        {
+            long n = VisibleDistanceN(p, im);
+            long s = VisibleDistanceS(p, im);
+            long w = VisibleDistanceW(p, im);
+            long e = VisibleDistanceE(p, im);
+            long product = n * s * w * e;
+            if (product > result)
+                result = product;
+        }
+        return result;
     }
+
+    private static bool IsVisibleN(KeyValuePair<(int r, int c), int> p, Im2 im)
+    {
+        int c = p.Key.c;
+        int r = p.Key.r;
+        int v = p.Value;
+
+        while (--r >= 0)
+        {
+            if (v <= im[(r, c)])
+                return false;
+        }
+
+        return true;
+    }
+
+    private static bool IsVisibleS(KeyValuePair<(int r, int c), int> p, Im2 im)
+    {
+        int c = p.Key.c;
+        int r = p.Key.r;
+        int v = p.Value;
+
+        while (++r < im.ROWS)
+        {
+            if (v <= im[(r, c)])
+                return false;
+        }
+
+        return true;
+    }
+
+    private static bool IsVisibleW(KeyValuePair<(int r, int c), int> p, Im2 im)
+    {
+        int c = p.Key.c;
+        int r = p.Key.r;
+        int v = p.Value;
+
+        while (--c >= 0)
+        {
+            if (v <= im[(r, c)])
+                return false;
+        }
+
+        return true;
+    }
+
+
+    private static bool IsVisibleE(KeyValuePair<(int r, int c), int> p, Im2 im)
+    {
+        int c = p.Key.c;
+        int r = p.Key.r;
+        int v = p.Value;
+
+        while (++c < im.COLS)
+        {
+            if (v <= im[(r, c)])
+                return false;
+        }
+
+        return true;
+    }
+
+
+    private static int
+        VisibleDistanceN(KeyValuePair<(int r, int c), int> p, Im2 im)
+    {
+        int visible = 0;
+        int c = p.Key.c;
+        int r = p.Key.r;
+        int v = p.Value;
+        while (--r >= 0)
+        {
+            visible++;
+            if (im[(r, c)] >= v)
+                break;
+        }
+
+        return visible;
+    }
+
+    private static int
+        VisibleDistanceS(KeyValuePair<(int r, int c), int> p, Im2 im)
+    {
+        int visible = 0;
+        int c = p.Key.c;
+        int r = p.Key.r;
+        int v = p.Value;
+
+        while (++r < im.ROWS)
+        {
+            visible++;
+            if (im[(r, c)] >= v)
+                break;
+        }
+
+        return visible;
+    }
+
+
+    private static int
+        VisibleDistanceW(KeyValuePair<(int r, int c), int> p, Im2 im)
+    {
+        int visible = 0;
+        int c = p.Key.c;
+        int r = p.Key.r;
+        int v = p.Value;
+
+        while (--c >= 0)
+        {
+            visible++;
+            if (im[(r, c)] >= v)
+                break;
+        }
+
+        return visible;
+    }
+
+
+    private static int
+        VisibleDistanceE(KeyValuePair<(int r, int c), int> p, Im2 im)
+    {
+        int visible = 0;
+        int c = p.Key.c;
+        int r = p.Key.r;
+        int v = p.Value;
+
+        while (++c < im.COLS)
+        {
+            visible++;
+            if (im[(r, c)] >= v)
+                break;
+        }
+
+        return visible;
+    }
+
 }
---
+++ utilities/image/Im2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AdventOfCode_2022.utilities.image;
+
+using Image = Dictionary<(int r, int c), int>;
+
+
+public class Im2
+{
+    public Im2(IEnumerable<string> s)
+    {
+        ROWS = s.Count();
+        COLS = s.First().Length;
+
+        int r = 0;
+        foreach (string row in s)
+        {
+            int c = 0;
+            foreach (var p in row)
+            {
+                Image[(r, c)] = p - '0';
+                c++;
+            }
+            r++;
+        }
+    }
+    public Image Image { get; } = new Image();
+    public int ROWS { get; }
+    public int COLS { get; }
+    public int this[(int r, int c) p]
+    {
+        get => Image[p];
+        set => Image[p] = value;
+    }
+}
+
